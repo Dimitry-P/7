9c2bc5569d84530b8af1406ce32d9f530c31b07f
Dimitry-P <dimitry.piatnitsky@yahoo.com>
2024-12-23 16:49:50 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_7
+{
+    //•	Задача 7: Напишите программу, которая вычисляет квадрат числа, введенного пользователем.
+    internal class Program
+    {
+        
+        public static int y = 2;
+        public static string number;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            number = Console.ReadLine();
+            int result = int.Parse(number);
+            
+            Console.WriteLine("Квадрат числа равен "+ Math.Pow(result, y));
+            Console.ReadKey();  
+        }
+    }
+}
